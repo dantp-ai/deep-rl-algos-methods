f83e88f50106a90f7a0fe305ca0ea222a6033c8f
daniel <1534513+dantp-ai@users.noreply.github.com>
2024-04-12 23:58:03 +0200

    Add PWNorthEastPolicy

--- agents/policy.py
+++ agents/policy.py
@@ -13,3 +13,9 @@ class RandomChainPolicy(BasePolicy):
         right = 1
         return jnp.random.choice([left, right])
 
+class PWNorthEastPolicy(BasePolicy):
+    def get_action(self, observation):
+        north = 3
+        east = 1
+        return jnp.random.choice([north, east])
+
